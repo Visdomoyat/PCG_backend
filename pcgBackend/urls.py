c1f7d8a45e481bc094ac8e82887257c06e349ecb
visdomoyat <wisdomoyatokun@gmail.com>
2026-04-28 13:26:29 -0400

    Fearure changes; from main_app to content_app

--- pcgBackend/urls.py
+++ pcgBackend/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main_app.urls')),
-    path('', include('CRM_app.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("content_app.urls")),
+    # path("", include("CRM_app.urls")),
 ]
